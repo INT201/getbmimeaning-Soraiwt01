f6ae6a05dcf0ef33c0838c983310ef528eb919c4
Sorawit Taochoo <sorawit.tar@mail.kmutt.ac.th>
2022-09-04 15:21:40 +0700

    Go

--- getBMIMeaning.js
+++ getBMIMeaning.js
@@ -1,9 +1,19 @@
 const { template } = require('@babel/core')
 
 function calculateBMI(weight, height) {
-  //code here
+ return weight/(height*height); //code here 
 }
 function getBMIMeaning(weight, height) {
-  //code here
+  let BMI = calculateBMI(weight, height)
+
+  if (BMI < 18.5){
+    return "Underweight";
+  }else if(BMI >18.5 && BMI <= 24.9){
+    return "Normal weight";
+  }
+  else{
+    return "Overweight";
+  } 
 }
 module.exports = getBMIMeaning
+
